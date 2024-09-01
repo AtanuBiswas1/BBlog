49f17c7c670ee508d77126f39b81402214c015a9
AtanuBiswas1 <atanubiswas341@gmail.com>
2024-09-01 22:35:39 +0530

    somechange

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -13,9 +13,7 @@ export default function RTE({ name, control, label, defaultValue = "" }) {
         control={control}
         render={({ field: { onChange } }) => (
           <Editor
-            // apiKey={import.meta.env.VITE_RTE_ID}
-            // apiKey={conf.rteapikey}
-            apiKey="ycc790it3fecfh6fq6bzf5kkf4atyotwyfpxj4lp8g6nqkwh"
+            apiKey={conf.rteapikey}
             initialValue={defaultValue}
             init={{
               initialValue: defaultValue,
