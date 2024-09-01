c5415813d8c860e636e7e700bc7950fbc5842ce5
AtanuBiswas1 <atanubiswas341@gmail.com>
2024-09-01 20:11:28 +0530

    Bblog website add into github

---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Logo({width = '100px'}) {
+  return (
+    <div>
+      <h1 className='text-green-600 font-extrabold text-[30px]'>BBlog</h1>
+    </div>
+  )
+}
+
+export default Logo
